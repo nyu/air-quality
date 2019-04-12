25ecf47f12e176ee7ce1917439b605463279f54a
LockonS483 <rumblie.gg@gmail.com>
2019-04-12 17:34:32 -0400

    teapots

--- server.js
+++ server.js
@@ -1,15 +1,48 @@
 // server.js
 
 const express        = require('express');
-const MongoClient    = require('mongodb').MongoClient;
+const Sequelize		 = require('sequelize');
 const bodyParser     = require('body-parser');
 const app            = express();
 
 const port = 8000;
 
+const sequelize 	 = new Sequelize('AirQualityData', 'iothosti', 'PM2.5', {
+	host:'localhost',
+	dialect: 'sqlite',
+	logging: false,
+	operatorsAliases: false,
+
+	storage: 'AirQualityData.sqlite',
+});
+
+const Dataset = sequelize.define('dataset', {
+	location: Sequelize.STRING,
+	time: Sequelize.STRING,
+	timezone: Sequelize.STRING,
+	name: Sequelize.STRING,
+	temp: Sequelize.STRING,		//Temperature
+	humidity: Sequelize.STRING,	//Humidity
+	////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////
+	pm2_5: Sequelize.STRING,
+	pm5: Sequelize.STRING,
+	CO2: Sequelize.STRING,		//Carbone Dioxide
+	CO: Sequelize.STRING,		//Carbone Monoxide
+	O2: Sequelize.STRING,
+	CH4: Sequelize.STRING,		//Methane
+	SO2: Sequelize.STRING,
+	H2S: Sequelize.STRING,
+	NO: Sequelize.STRING,
+	NO2: Sequelize.STRING,
+	O3: Sequelize.STRING,
+	AH: Sequelize.STRING		//Absolute Humidity
+});
+
 app.use(bodyParser.urlencoded( {extended: true} ))
 
 require('./app/routes')(app, {});
+Dataset.sync();
 app.listen(port, ()=> {
 	console.log('we are live on ' + port);
 });
