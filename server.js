f5f499fd376a95181a41a54916a6b16479bdf027
Kognise <felix.mattick@gmail.com>
2019-06-17 12:11:26 -0400

    Send empty initial response if no data

--- server.js
+++ server.js
@@ -143,6 +143,7 @@ nextApp.prepare().then(async () => {
       return
     }
     const point = await Point.findOne().sort('-when')
+    if (!point) return res.end()
     const filtered = filterFields(point, pointSchema)
     res.json(addTimezones(filtered))
   })
