4635f6e49c5dcaffbe60362bb686f4d629ae1f63
caspar <caspar@nyu.edu>
2019-11-07 19:34:02 -0500

    POC AQI API

--- components/Text.js
+++ components/Text.js
@@ -1,10 +1,11 @@
-export default (props) => (
+export default ({ color, children }) => (
   <p>
-    {props.children}
+    {children}
     <style jsx>{`
       p {
         margin: 0;
         margin-bottom: 12px;
+        color: ${color || 'inherit'};
       }
     `}</style>
   </p>
---
+++ pages/api/aqi.js
@@ -0,0 +1,3 @@
+export default (req, res) => {
+  res.json({ aqi: 400 })
+}
