604dea69f00e7b5ad2ecc486655c4b71185893aa
caspar <caspar@nyu.edu>
2019-11-11 21:09:10 -0500

    Changed fonts to Gotham (official NYU typeface). Kinda hate it though. Thoughts?

--- components/Layout.js
+++ components/Layout.js
@@ -3,7 +3,7 @@ import Title from './Title'
 export default ({ children }) => {
   return (
     <main>
-      <Title>Air Quality Monitor</Title>
+      <Title>Shanghai Air Quality</Title>
 
       {children}
 
@@ -20,12 +20,12 @@ export default ({ children }) => {
         @font-face {
           font-family: 'Circular Std';
           font-weight: 400;
-          src: url('/static/CircularStd-Book.ttf');
+          src: url('/static/font/Gotham-light.otf');
         }
         @font-face {
           font-family: 'Circular Std';
           font-weight: 700;
-          src: url('/static/CircularStd-Bold.ttf');
+          src: url('/static/font/Gotham-book.otf');
         }
         body {
           background-color: #2E3440;
